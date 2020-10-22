24cfcb0e7a728dfd72c3b1a68c0675f0e1d43a23
Edward Ramirez <edal_ramirez@hotmail.com>
2020-10-22 08:55:49 -0500

    Redirecting the Post Request and Adding a Flash Message
    Given that I didn't specify an url in the action form attribute, the form posts back the data in the same route, so
    I had to put an if statement that handles when the request method is post and when the method is get. I can access the
    method with request.method. If that happens, I want to create a form with the request.POST data, then validate the form
    data with the is_valid() method.
    
    Once the form is validated, I grab the userName from the cleaned_data form dictionary key called 'username' and then
    I created a success flash message. Finally, I redirect the user to the blog url. The messages are sent without putting
    them in any context.
    
    In the html base template (because I want to put flash messages at all the web pages) I added an if statement that
    checks the messages, then loop through the messages and put every single message in a div tag that has some Bootstrap
    classes (alert and alert-type of flash message). Since the flash message type in Django is the same as Bootstrap, I
    passed the message tags attribute in the class

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm
-    return render(request, 'users/register.html', {'form': form})
+    if request.method == "POST":
+        # Si es POST, voy a validar los datos
+        # Crear un form con los datos de la peticion
+        form = UserCreationForm(request.POST)
+        # Los datos de la peticion puede ser cualquier cosa
+        # Por tanto se va a validar que se obtienen los datos deseados
+        if form.is_valid():
+            # obtener el nombre de usuario que se obtuvo
+            # los datos validados del form van a estar en el diccionario
+            # cleaned_Data
+            userName = form.cleaned_data.get('username')
+            # Colocar un flash message para mostrar que se obtuvieron buenos datos.
+            # Un mensaje flash es una buena manera de mostrar alertas
+            # Que se muestran una sola vez y que desparecen en la siguiente
+            # peticion. Que tipo de mensaje quiero mostrar(succes, info, warning)
+            messages.success(request, f"Account created for {userName}!")
+            # Redirigir al usuario
+            return redirect('blog:blog-home')
+    else:
+        # Blank form
+        form = UserCreationForm()
+        return render(request, 'users/register.html', {'form': form})
