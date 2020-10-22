4b8d32eb195e5dabf5fa9ad95fa4179710db0528
Edward Ramirez <edal_ramirez@hotmail.com>
2020-10-21 21:58:16 -0500

    Creating the User Registration Form
    Given that creating a user registration form is a frecuent task, Django provides a class that make that form for me, so
    I had to import the UserCreationForm class and cosntruct an object from that blueprint within the view. Then, I render
    the template passing the form object in the context.
    
    The url pattern that I made was 'register/' and the view that handles this pattern is register from users app. This time,
    I wanted to set up the urlpattern in the main urls.py file, so I imported the views.py from users folder as users_views
    because I could made more patterns in that file in the future so naming views.py that way allows me to be relaxed and
    have code flexibility

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 # Create your views here.
-
-post_list = [
-    {
-        'author': 'Edward Ramirez',
-        'Title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Aug 12 2016'
-    },
-    {
-        'author': 'Ward',
-        'Title': 'Django Tutorial',
-        'content': 'In this tutorial I\'m covering the templates',
-        'date_posted': 'Aug 13 2016'
-    }
-
-]
 
 
 def home(request):
     # Mediante la llave post list voy a acceder a un objeto python
     # Que en este caso es una lista de diccionarios
     context = {
-        'post_list': post_list
+        'post_list': Posts.objects.all()
 
     }
     return render(request, 'blog/home.html', context)
