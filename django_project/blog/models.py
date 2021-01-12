150685966875405c83987c09bbe660ace6e5ef42
Edward Ramirez <edal_ramirez@hotmail.com>
2021-01-12 14:47:38 -0500

    Adding the image feature to the post
    MODEL:
    I added the ImageField attribute. The null means that Django doesn't need to store the image and Blank means that
    the form can pass validation without the image
    'image = models.ImageField(upload_to='post_pic', null=True, blank=True)'
    
    FORM and VIEWS:
    I jus had to add the image string in 'fields' attribute, given that the update and create views are responsible for
    the form creation
    
    fields = ['title', 'content', 'image']
    
    POST  TEMPLATE:
    I can access the image url through the post object like this: 'post.image.url'. Then, I put the image HTML tag to the
    post article giving it some Bootstrap classes to make it prettier
    CODE
    {% if post.image %}
        <img src="{{ post.image.url }}" class="car-img-top img-fluid" alt="{{ post }} Pic"/>
        {% endif %}

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,12 +3,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_pic', null=True, blank=True)
     date_posted = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(
+        User, through='Comment', related_name='posts_comments')
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    # related name means, give me all the comments of this user
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="all_posts_comments")
+    # dame todos los comentarios de este post
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='all_comments')
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        if len(self.text) > 15:
+            return self.text[:15]
+        else:
+            return self.text
