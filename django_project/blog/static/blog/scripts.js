da781912dd10d0531f4497d3bed79869f2a3a2a6
Edward Ramirez <edal_ramirez@hotmail.com>
2021-01-14 22:06:18 -0500

    Adding a new block to the base template
    At the end of the body tag I added the '{% block scripts %}' in order to put the JavaScripts files there. I also
    removed the script tags and I put the code in 'blog/scripts.js' at the static folder because I wanted to organize
    and separate the JavaScript code.

---
+++ django_project/blog/static/blog/scripts.js
@@ -0,0 +1,47 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function sendPost(url) {
+    const csrftoken = getCookie('csrftoken');
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("X-CSRFToken", csrftoken);
+    xhttp.send();
+}
+    
+
+function handleFavorites(url, url2, postID) {
+    var anchor = document.getElementById('fav-'+postID);
+    if (anchor.className.includes("btn-favorite")){
+        sendPost(url2);
+        anchor.classList.replace("btn-favorite", "btn-no-favorite");
+    } else {
+        sendPost(url);
+        anchor.classList.replace("btn-no-favorite", "btn-favorite");
+    }
+}
+    
+function editComment(url, commentID) {
+    var ul = document.getElementsByClassName("list-group")[0];
+    var li = document.getElementById('comment-' + commentID);
+    var text = document.getElementById('comment-text-' + commentID).innerHTML;
+    var input = document.getElementById('id_text');
+    input.value = text;
+    ul.removeChild(li);
+    // sending the post request
+    var form = document.getElementsByTagName("form")[1];
+    form.action = url;
+}
