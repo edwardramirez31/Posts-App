54d44d8f26951c80ec91570ad9078e7a9acbd654
Edward Ramirez <edal_ramirez@hotmail.com>
2021-05-05 13:16:16 -0500

    Adding search functionality
    I added search functionality to the profile and notifications list view, so that the user can get the notifications sent by some specific user
    and the user also can get the user profile by its username or email in the searchbar.
    
    In order to do this, I had to use the Q filter, passing the field (username and email) and the lookup query that I wanted to do, in this case,
    icontains. Then, I pass that query to the filter method used in the context manager. This search functionality is also handled by the view
    getting the search key from the url if this exists with request.GET.get("search", False).

--- django_project/notifications/views.py
+++ django_project/notifications/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from .models import Notification
+from django.db.models import Q
+
 # Create your views here.
 def get_notifications(request):
-    notifications = request.user.get_all_notifications.all().order_by('-date')
+    search_value = request.GET.get('search', False)
+    if search_value:
+        query = Q(sender__username__icontains=search_value)
+        notifications = Notification.objects.filter(query).select_related().order_by('-date')
+    else:
+        notifications = request.user.get_all_notifications.all().order_by('-date')
+        
     paginator = Paginator(notifications, 20)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -3,6 +3,13 @@ from django.views import View
 from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http import HttpResponse
+from .models import Profile
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
 # Create your views here.
 class Register(View):
     def get(self, request):
@@ -22,23 +29,69 @@ class Register(View):
         return redirect('blog:home')
 
 
-from django.contrib.auth.decorators import login_required
 
 @login_required
 def profile(request, pk):
     profile = get_object_or_404(User, pk=pk)
+    followers = profile.all_followers.all()
+    following = profile.profile.following.all()
+    posts = profile.post_set.all()
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {"user_": profile, "page_obj": page_obj, "followers": followers, "following": following}
+    return render(request, 'users/profile.html', context)
+
+
+@login_required
+def update_profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
+            left = float(request.POST.get('x'))
+            top = float(request.POST.get('y'))
+            right = left + float(request.POST.get('width'))
+            bottom = top + float(request.POST.get('height'))
+            coords = (left, top, right, bottom)
             user_form.save()
-            profile_form.save()
+            instance = profile_form.save(commit=False)
+            instance.save(coordinates=coords)
             messages.success(request, f'Your profile has been updated.')
-            return redirect('blog:home')
+            return redirect(reverse('profile', args=[request.user.id]))
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
-    context = {"user_form": user_form, "profile_form": profile_form, "user_": profile}
-    return render(request, 'users/profile.html', context)
+    context = {"user_form": user_form, "profile_form": profile_form}
+    return render(request, 'users/update_profile.html', context)
+
+
+def follow(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    profile = Profile.objects.get(user=request.user)
+    profile.following.add(user)
+    return HttpResponse()
+
+def unFollow(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    profile = Profile.objects.get(user=request.user)
+    profile.following.remove(user)
+    return HttpResponse()
+
+def profiles(request):
+    search_value = request.GET.get('search', False)
+    if search_value:
+        query = Q(username__icontains=search_value) | Q(email__icontains=search_value)
+        users = User.objects.filter(query).select_related()
+    else:
+        profile = Profile.objects.get(user=request.user)
+        all_users = set(User.objects.all().exclude(username=request.user.username))
+        following = set(profile.following.all())
+        users = all_users.difference(following)
+        
+    context = {'users': users}
+    return render(request, 'users/profiles.html', context)
+
 
 # This method creates and saves a database object from the data bound to the form. A subclass of ModelForm can accept an existing model instance as the keyword argument instance; if this is supplied, save() will update that instance. If it’s not supplied, save() will create a new instance of the specified model:
